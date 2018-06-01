8dc800b399c83be26f2c519f64c4f8e1bacea5d2
trrttrtrsrfewrwed <timurruskiy2@mail.ru>
2018-06-01 14:36:29 +0300

    1st commit :)

---
+++ src/2.kt
@@ -0,0 +1,28 @@
+import java.util.*
+data class Player( var name : String, var xp : Double = 0.0,var maxHP : Double = 10.0, var hp : Double = 100.0, var lvl : Int = 1,
+                   var NextLvlXP : Double = 30.0)
+{
+    fun deathPenalty()
+    {
+        xp /= 2
+        hp = 5.0
+    }
+
+    fun gainXP(value : Double)
+    {
+        if (NextLvlXP < xp + value)
+        {
+            lvl++
+            xp = 0.0
+            NextLvlXP = NextLvlXP * 2
+        }
+    }
+
+
+}
+
+
+fun main(args: Array<String>){
+    val player = Player("hero1")
+    player.gainXP(5.0)
+}
